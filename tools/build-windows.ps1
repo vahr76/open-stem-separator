a5798035255be4a21588fe5586fa748ff86427a1
vahr76 <vahr76@gmail.com>
2026-09-20 23:15:07 -0300

    Bump package artifact version to 0.1.2

--- tools/build-windows.ps1
+++ tools/build-windows.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 
 $AppName = "ytd"
-$AppVersion = "0.1.1"
+$AppVersion = "0.1.2"
 $Target = "windows-x64"
 $PackageName = "$AppName-$AppVersion-$Target"
 $RootDir = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)
@@ -55,7 +55,7 @@ setlocal EnableExtensions
 
 set "APP_NAME=ytd"
 set "APP_VERSION=0.1.0"
-set "PACKAGE_NAME=ytd-0.1.1-windows-x64"
+set "PACKAGE_NAME=ytd-0.1.2-windows-x64"
 set "SOURCE_DIR=%~dp0"
 set "INSTALL_ROOT=%LOCALAPPDATA%\OSS"
 set "INSTALL_DIR=%INSTALL_ROOT%\%PACKAGE_NAME%"
@@ -102,7 +102,7 @@ function Write-UninstallCmd($PayloadDir) {
 @echo off
 setlocal EnableExtensions
 
-set "PACKAGE_NAME=ytd-0.1.1-windows-x64"
+set "PACKAGE_NAME=ytd-0.1.2-windows-x64"
 set "INSTALL_ROOT=%LOCALAPPDATA%\OSS"
 set "INSTALL_DIR=%INSTALL_ROOT%\%PACKAGE_NAME%"
 set "BIN_DIR=%LOCALAPPDATA%\OSS\bin"
